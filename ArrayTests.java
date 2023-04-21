88cb4111133aba0a839445c411396ece6b95f531
Kent Giang <kentgi154@gmail.com>
2023-04-20 17:48:18 -0700

    need to finish listtests

--- ArrayTests.java
+++ ArrayTests.java
@@ -27,4 +27,10 @@ public class ArrayTests {
     int[] input2 = {3, 2, 1};
     assertArrayEquals(new int[]{1, 2, 3}, ArrayExamples.reversed(input2));
   }
+
+  @Test
+  public void testAverageWithoutLowest() {
+    double[] input1 = {2.0, 2.0, 2.0};
+    assertEquals(0.0, ArrayExamples.averageWithoutLowest(input1), 0);
+  }
 }
---
+++ ListTests.java
@@ -0,0 +1,16 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListTests {
+    @Test
+    public void testFilter() {
+        List<String> inputList1 = Arrays.asList("dog", "cat", "cow");
+        List<String> outputList1 = Arrays.asList("cat", "cow");
+        StringChecker sc1 = "c";
+
+        AssertEquals(output, );
+    }
+    
+}
